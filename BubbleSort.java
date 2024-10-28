a62b731dfe196dfb217049ed5171b7d10735d132
EvgeniaPotato <160453277+EvgeniaPotato@users.noreply.github.com>
2024-10-28 11:46:37 +0900

    Add files via upload

---
+++ BubbleSort.java
@@ -0,0 +1,29 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] array = {2, 61, 25, 12, 22, 11, 0};
+        bubbleSort(array);
+        System.out.println("Sorted massive:");
+        printArray(array);
+    }
+
+    public static void bubbleSort(int[] array) {
+        int n = array.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    // Меняем array[j] и array[j+1]
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArray(int[] array) {
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+        System.out.println();
+    }
+}
